fbc785268959120977f153d547e8b8994eb1b329
WatsonLi <watsonli@t3ex-group.com>
2025-06-17 17:56:42 +0800

    test ripple and tailwind

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,10 +1,13 @@
 import { Validators, FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
-// import { MatRippleModule } from '@angular/material/core';
+import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatRippleModule, MatIconModule, MatDividerModule, MatButtonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
